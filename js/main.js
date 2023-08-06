34e74dedbd71b2f54b747e293924e24d45be4106
Marina Gaibovich <marina.gaibovich@gmail.com>
2023-08-06 23:56:32 +0300

    Big Commit

--- js/main.js
+++ js/main.js
@@ -5,34 +5,20 @@ $(document).ready(function(){
       adaptiveHeight:true,
       speed:500,
       easing:'ease',
+      swipe:true,
+      appenDots:$('.slider__content'),
    });
     
 
 });
-const videoBtn = document.querySelector('#video-story-btn');
-const videoBtnIcon = document.querySelector('#video-story-btn-icon');
-const videoOverlay = document.querySelector('#video-story-overlay');
-const videoFile = document.querySelector('#video-story');
+  
 
-videoBtn.addEventListener('click', function() {
-    function toggleOverlay(event) {
-        if (event.type === 'mouseleave') {
-            videoOverlay.classList.add('hidden');
-        }
+const menuBtn = document.querySelector('.menu__btn');
+const menu = document.querySelector('.menu__list');
 
-    }
+menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('menu__list--active');
 
-    if (videoFile.paused) {
-        videoFile.play();
-        videoBtnIcon.src = 'image/pause-white.svg';
-
-        videoOverlay.onmouseleave = toggleOverlay;
-        videoOverlay.onmouseenter= toggleOverlay;
-
-    } else {
-        videoFile.pause();
-        videoBtnIcon.src = 'image/play.svg';
-        videoOverlay.onmouseleave = null;
-        videoOverlay.onmouseenter = null;
-    }
 });
+
+
