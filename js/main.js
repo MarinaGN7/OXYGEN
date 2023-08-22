4e07183e12820a35b0670e091724e0bd1bb44b55
Marina Gaibovich <marina.gaibovich@gmail.com>
2023-08-22 23:26:06 +0300

    Story

--- js/main.js
+++ js/main.js
@@ -5,9 +5,9 @@ $(function(){
       arrows:true,
       responsive:[
          {
-            breakpoint: 767.98,
+            breakpoint: 555,
             settings: {
-               dots:false,
+               arrows:false,
             }
          }
       ]
@@ -26,4 +26,31 @@ menuBtn.addEventListener('click', () => {
 
 });
 
+const videoBtn = document.querySelector('#video-story-btn');
+const videoBtnIcon = document.querySelector('#video-story-btn-icon');
+const videoOverlay = document.querySelector('#video-story-overlay');
+const videoFile = document.querySelector('#video-story');
 
+videoBtn.addEventListener('click', function() {
+   function toggleOverlay(event) {
+      if (event.type === 'mouseLeave') {
+         videoOverlay.classList.add('hidden');
+      } else {
+         videoOverlay.classList.remove('hidden');
+      }
+   }
+   
+   if (videoFile.paused) {
+      videoFile.play();
+      videoBtnIcon.src = 'image/pause.svg';
+      
+      videoOverlay.onmouseleave = toggleOverlay;
+      videoOverlay.onmouseenter = toggleOverlay;
+
+   } else {
+      videoFile.pause();
+      videoBtnIcon.src = 'image/play.svg';
+      videoOverlay.onmouseleave = null;
+      videoOverlay.onmouseenter = null;
+   }
+});
