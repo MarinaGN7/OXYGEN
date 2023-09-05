7225868fc2a2f0337cebd6392a4d8f541fe8cbc5
Marina Gaibovich <marina.gaibovich@gmail.com>
2023-09-05 12:16:51 +0300

    Header

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-$(function(){
+$(function () {
 
    $('.slider-slider').slick({
-      dots:true,
-      arrows:true,
-      responsive:[
+      dots: true,
+      arrows: true,
+      responsive: [
          {
             breakpoint: 555,
             settings: {
-               arrows:false,
+               arrows: false,
             }
          }
       ]
@@ -15,42 +15,54 @@ $(function(){
 
 });
 
+$(document).ready(function() {
+   $('.header__burger').click(function(event) {
+      $('.header__burger,.header__menu').toggleClass('active')
 
-  
 
-const menuBtn = document.querySelector('.menu__btn');
-const menu = document.querySelector('.menu__list');
-
-menuBtn.addEventListener('click', () => {
-    menu.classList.toggle('menu__list--active');
+   });
 
 });
+// $(function () {
 
-const videoBtn = document.querySelector('#video-story-btn');
-const videoBtnIcon = document.querySelector('#video-story-btn-icon');
-const videoOverlay = document.querySelector('#video-story-overlay');
-const videoFile = document.querySelector('#video-story');
+   // const navBtn = document.querySelector('.nav__btn');
+   // const nav = document.querySelector('.nav__items');
 
-videoBtn.addEventListener('click', function() {
-   function toggleOverlay(event) {
-      if (event.type === 'mouseLeave') {
-         videoOverlay.classList.add('hidden');
-      } else {
-         videoOverlay.classList.remove('hidden');
+   // navBtn.addEventListener('click', () => {
+      // nav.classList.toggle('.nav__items--active');
+   // });
+
+// });
+
+$(function () {
+
+   const videoBtn = document.querySelector('#video-story-btn');
+   const videoBtnIcon = document.querySelector('#video-story-btn-icon');
+   const videoOverlay = document.querySelector('#video-story-overlay');
+   const videoFile = document.querySelector('#video-story');
+
+   videoBtn.addEventListener('click', function () {
+      function toggleOverlay(event) {
+         if (event.type === 'mouseLeave') {
+            videoOverlay.classList.add('hidden');
+         } else {
+            videoOverlay.classList.remove('hidden');
+         }
       }
-   }
-   
-   if (videoFile.paused) {
-      videoFile.play();
-      videoBtnIcon.src = 'image/pause.svg';
-      
-      videoOverlay.onmouseleave = toggleOverlay;
-      videoOverlay.onmouseenter = toggleOverlay;
 
-   } else {
-      videoFile.pause();
-      videoBtnIcon.src = 'image/play.svg';
-      videoOverlay.onmouseleave = null;
-      videoOverlay.onmouseenter = null;
-   }
+      if (videoFile.paused) {
+         videoFile.play();
+         videoBtnIcon.src = 'image/pause.svg';
+
+         videoOverlay.onmouseleave = toggleOverlay;
+         videoOverlay.onmouseenter = toggleOverlay;
+
+      } else {
+         videoFile.pause();
+         videoBtnIcon.src = 'image/play.svg';
+         videoOverlay.onmouseleave = null;
+         videoOverlay.onmouseenter = null;
+      }
+   });
+   
 });
